ab762ef374ee58f86a863a458b1614c9c9e27cc1
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-20 19:52:47 +0100

    created Main.java as start poin of Application

---
+++ google/src/com/news/searcher/Main.java
@@ -0,0 +1,9 @@
+package com.news.searcher;
+
+import com.news.searcher.service.Executor;
+
+public class Main {
+    public static void main(String[] args) {
+        new Executor().startApp();
+    }
+}
