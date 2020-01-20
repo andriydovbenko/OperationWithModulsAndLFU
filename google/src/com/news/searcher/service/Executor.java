b103222b06a0f6151c322c8301685599d425b13e
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-20 19:52:06 +0100

    created Executor.java inside service package to run App

---
+++ google/src/com/news/searcher/service/Executor.java
@@ -0,0 +1,66 @@
+package com.news.searcher.service;
+
+import com.news.searcher.model.LFUCache;
+import com.news.searcher.repository.Repository;
+
+import java.util.*;
+
+public class Executor {
+    private Scanner scanner;
+    private LFUCache lfuCacheList;
+    private List<String> allEvents;
+    private List<String> checkList;
+
+    public Executor() {
+        Repository repository = new Repository();
+        this.lfuCacheList = repository.getLfuList();
+        this.allEvents = repository.getAllEvents();
+        this.checkList = repository.getCheckList();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void startApp() {
+        printAllEventsList();
+        startSelectingEventsFromConsole();
+        printFLUCacheListIntoConsole();
+    }
+
+    private void printAllEventsList() {
+        for (int i = 0; i < allEvents.size(); i++) {
+            System.out.println("event key= " + (i + 1) + " " + allEvents.get(i));
+        }
+    }
+
+    private void startSelectingEventsFromConsole() {
+        System.out.println("\nTo stop selecting events you need to enter: 'exit'\n");
+        boolean inputStatus = true;
+        while (inputStatus) {
+            String input = inputEventFromConsole(scanner);
+            inputStatus = putTheEventIntoMap(input);
+        }
+    }
+
+    private boolean putTheEventIntoMap(String input) {
+        boolean inputStatus = true;
+        if (input.equals("exit")) {
+            inputStatus = false;
+            scanner.close();
+        } else if (checkList.contains(input)) {
+            System.out.println("exist");
+            int item = Integer.parseInt(input) - 1;
+            lfuCacheList.put(input, allEvents.get(item));
+        } else {
+            System.out.println("Incorrect input. Try again");
+        }
+        return inputStatus;
+    }
+
+    private String inputEventFromConsole(Scanner scanner) {
+        return scanner.next();
+    }
+
+    private void printFLUCacheListIntoConsole() {
+        System.out.println("\n" + Repository.getCAPACITY() + " the most popular news :\n" +
+                lfuCacheList.toString());
+    }
+}
